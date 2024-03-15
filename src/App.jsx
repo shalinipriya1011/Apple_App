10bd3bb870a3b7704e973f73e0c7477169b6d083
shalinipriya1011 <70254428+shalinipriya1011@users.noreply.github.com>
2024-03-16 00:29:18 +0530

    added features

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
 
 
 const App = () => {
+return (
+   <main className ="bg-black">
+   <Navbar /> 
+   <Hero />
+   <Highlights />
 
-  return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+   </main>
   )
 }
 
